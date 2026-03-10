362be69f5baeb8feb06adcde2df759e654c557ff
r3gulus-4rcturus <muh.lanangzalkifla@gmail.com>
2026-03-10 09:47:36 +0700

    [RED] Add payment model skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import java.util.Map;
+
+import lombok.Getter;
+
+@Getter
+public class Payment {
+    String id;
+    String method;
+    String status;
+    Map<String, String> paymentData;
+}
